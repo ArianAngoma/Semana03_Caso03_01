9f798a0d7bbcd9fe944d9ef440c4023ce005d834
unknown <arian.angoma@tecsup.edu.pe>
2019-08-26 23:15:05 -0500

    first commit

---
+++ Caso03_01/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Caso03_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Caso03_01"].ConnectionString);
+
+        private void Dg01_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("UspListaClientes", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaAnios");
+
+                        Dg01.DataSource = df.Tables["ListaAnios"];
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+        }
+    }
+}
